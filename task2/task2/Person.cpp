fe4bdb5602f70b562e6a0a2a99001ae187c5ffa5
DenisTuxvatullin <den44icc@mail.ru>
2016-12-08 17:42:13 +0300

    class Person

---
+++ task2/task2/Person.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "Person.h"
+
+
+CPerson::CPerson(unsigned short age, string name, unsigned short weight, unsigned short growth, bool isMale)
+{
+
+}
+
+CPerson::~CPerson()
+{
+}
+
+unsigned short CPerson::GetAge()const
+{
+	return p_age;
+}
+void CPerson::SetAge(unsigned short age)
+{
+	if (age > p_age)
+	{
+		p_age = age;
+	}
+}
+
+unsigned short CPerson::GetWeight()const
+{
+	return p_weight;
+}
+void CPerson::SetWeight(unsigned short weight)
+{
+	if (weight > 0)
+	{
+		p_weight = weight;
+	}
+}
+
+unsigned short CPerson::GetGrowth()const
+{
+	return p_growth;
+}
+void CPerson::SetGrowth(unsigned short growth)
+{
+	if (growth > p_growth)
+	{
+		p_growth = growth;
+	}
+}
+
+string CPerson::GetName()const
+{
+	return p_name;
+}
+void CPerson::SetName(string name)
+{
+	p_name = name;
+}
